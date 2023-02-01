092f35a8db77b08543996e31da1164a462f40bab
olamideare <63550820+olamideare@users.noreply.github.com>
2023-02-01 16:54:15 +0100

    fix: null undefined

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -24,66 +24,71 @@ async function Post({ params: { slug } }: Props) {
   return (
     <article className="px-10 pb-28  max-w-5xl mx-auto">
       <section className="space-y-2 border border-[#3A76DA] text-white mb-8">
-        <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
-          <div className="absolute top-0 w-full h-full opacity-10 blur sm p-10 ">
-            {post?.mainImage && (
+        {post && (
+          <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
+            <div className="absolute top-0 w-full h-full opacity-10 blur sm p-10 ">
               <Image
                 className="object-cover object-center mx-auto"
-                src={urlFor(post?.mainImage).url()}
-                alt={post.author?.name}
+                src={post?.mainImage && urlFor(post.mainImage).url()}
+                alt={post.author?.name && post.author.name}
                 fill
               />
-            )}
-          </div>
+            </div>
 
-          <section className="p-5 bg-[#3A76DA]">
-            <div className="flex flex-col md:flex-row justify-between gap-y-5">
-              <div>
-                <h1 className="text-4xl font-extrabold">{post?.title}</h1>
-                <p>
-                  {new Date(post?._createdAt).toLocaleDateString("en-US", {
-                    day: "numeric",
-                    month: "long",
-                    year: "numeric",
-                  })}
-                </p>
-              </div>
-              <div className="flex items-center space-x-2">
-                {post.author?.image && (
+            <section className="p-5 bg-[#3A76DA]">
+              <div className="flex flex-col md:flex-row justify-between gap-y-5">
+                <div>
+                  <h1 className="text-4xl font-extrabold">
+                    {post?.title && post.title}
+                  </h1>
+                  <p>
+                    {new Date(post?._createdAt).toLocaleDateString("en-US", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
+                  </p>
+                </div>
+                <div className="flex items-center space-x-2">
                   <Image
                     className="rounded-full"
-                    src={urlFor(post.author?.image).url()}
-                    alt={post.author?.name}
+                    src={post.author?.image && urlFor(post.author.image).url()}
+                    alt={post.author?.name && post.author.name}
                     height={40}
                     width={40}
                   />
-                )}
 
-                <div className="w-64">
-                  <h3 className="text-lg font-bold">{post.author?.name}</h3>
-                  {/* <div>Author BIO</div> */}
+                  <div className="w-64">
+                    <h3 className="text-lg font-bold">
+                      {post.author?.name && post.author.name}
+                    </h3>
+                    {/* <div>Author BIO</div> */}
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div>
-              <h2 className="italic pt-10">{post?.description}</h2>
-              <div className="flex items-center justify-end ">
-                {post?.categories.map((category) => (
-                  <p
-                    key={category?._id}
-                    className="bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4"
-                  >
-                    {category?.title}
-                  </p>
-                ))}
+              <div>
+                <h2 className="italic pt-10">{post?.description}</h2>
+                <div className="flex items-center justify-end ">
+                  {post?.categories.map((category) => (
+                    <p
+                      key={category?._id}
+                      className="bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4"
+                    >
+                      {category?.title}
+                    </p>
+                  ))}
+                </div>
               </div>
-            </div>
-          </section>
-        </div>
+            </section>
+          </div>
+        )}
       </section>
 
-      <PortableText value={post?.body} components={RichTextComponents} />
+      <PortableText
+        value={post?.body && post.body}
+        components={RichTextComponents}
+      />
     </article>
   );
 }
